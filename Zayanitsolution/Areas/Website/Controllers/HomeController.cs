ba15e4035ca01a26a836acfb6846ea28e6d20ca7
Rezaul Islam <rezaul@royex.net>
2023-07-06 09:24:06 +0600

    update

--- Zayanitsolution/Areas/Website/Controllers/HomeController.cs
+++ Zayanitsolution/Areas/Website/Controllers/HomeController.cs
@@ -9,5 +9,80 @@ namespace zayanitsolution.Areas.Website.Controllers
         {
             return View();
         }
+
+        [HttpGet("services/erp-solution")]
+        public IActionResult Erp()
+        {
+            return View();
+        }
+
+        [HttpGet("services/customizes-software-development")]
+        public IActionResult CustomSoftware()
+        {
+            return View();
+        }
+
+        [HttpGet("services/website-development")]
+        public IActionResult WebsiteDevlopment()
+        {
+            return View();
+        }
+
+        [HttpGet("services/mobile-app-development")]
+        public IActionResult MobileAppDevelopment()
+        {
+            return View();
+        }
+
+        [HttpGet("services/ecommerce-platfrom-development")]
+        public IActionResult Ecommerce()
+        {
+            return View();
+        }
+
+        [HttpGet("services/it-consultation")]
+        public IActionResult ITConsultation()
+        {
+            return View();
+        }
+
+        [HttpGet("product/erp")]
+        public IActionResult ProductErp()
+        {
+            return View();
+        }
+
+
+        [HttpGet("product/smart-business")]
+        public IActionResult ProductSmartBusiness()
+        {
+            return View();
+        }
+
+
+        [HttpGet("product/accounting")]
+        public IActionResult ProductAccounting()
+        {
+            return View();
+        }
+
+
+        [HttpGet("product/ecommerce")]
+        public IActionResult ProductECommerce()
+        {
+            return View();
+        }
+
+        [HttpGet("aboutus")]
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
